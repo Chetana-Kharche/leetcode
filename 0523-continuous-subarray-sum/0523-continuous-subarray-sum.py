04a99e98560c27b157a27a38a6022d3057b81451
Chetana-Kharche <chetanavkharche@gmail.com>
2026-08-19 06:23:06 +0530

    Time: 54 ms (44.88%), Space: 39.4 MB (79.06%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        rem_map={0:-1}
+
+        prefix=0
+
+        for i in range(len(nums)):
+            prefix+=nums[i]
+
+            rem =prefix % k
+
+            if rem in rem_map:
+                if i-rem_map[rem]>=2:
+                    return True
+            else:
+                rem_map[rem]=i
+        return False
